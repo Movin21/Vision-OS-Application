0de96ee008120676bd2bfa617d30dc2d2ffbfeec
Movin21 <movinliyanage@gmail.com>
2026-06-02 05:31:59 +0530

    first commit

---
+++ NurseryConnectVision/NurseryConnectVision/NurseryConnectVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NurseryConnectVisionApp.swift
+//  NurseryConnectVision
+//
+//  Created by Movindu Liyanage on 2026-06-02.
+//
+
+import SwiftUI
+
+@main
+struct NurseryConnectVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
